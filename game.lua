203404eb1e1a33345cfff02692fe6ceb39d51c79
Alessandro Amatucci Girlanda <alexanderamatucci@hotmail.com>
2023-11-20 22:08:18 +0000

    Self changes in Game.lua
    Changed all of the global variables and functions in the Game.lua file into 'self' variable of the object 'Game'. This will use less global memory in the system and allow a better control and flexibility in the code.

--- game.lua
+++ game.lua
@@ -2,20 +2,21 @@ Game = Object:extend()
 
 function Game:new()  
     -- Check if player lost
-    game_over = false
+    self.game_over = false
+    self.player_dead = false
 
     -- Create Objects/Entities
-    player = Player(player_pic, WINDOW_WIDTH_CENTER - PLAYER_WIDTH / 2, WINDOW_HEIGHT - PLAYER_HEIGHT * 2)
+    self.player = Player(player_pic, WINDOW_WIDTH_CENTER - PLAYER_WIDTH / 2, WINDOW_HEIGHT - PLAYER_HEIGHT * 2)
 
     -- Create Projectiles
-    projectiles_timer = PROJECTILES_WAIT
-    projectiles = {}
+    self.projectiles_timer = PROJECTILES_WAIT
+    self.projectiles = {}
 
     -- Create Enemies
-    enemies_adjust = false
+    self.enemies_adjust = false
     local enemies_x_start = (WINDOW_WIDTH - (ENEMIES_COLS * ENEMY_WIDTH + (ENEMIES_COLS - 1) * ENEMIES_GAP)) / 2
     local enemies_y_start = 30
-    enemies_direction = 1
+    self.enemies_direction = 1
     
     -- Every half number of enemy rows (round up) gains one additional health
     local enemies_rows = {}
@@ -30,127 +31,119 @@ function Game:new()
         health = health + 1
     end
 
-    enemies = {}
+    self.enemies = {}
     for r=1,#enemies_rows do
-        tmp_enemies = {}
+        local tmp_enemies = {}
         for c=1,ENEMIES_COLS do
-            enemy = Enemy(enemies_pic, enemies_x_start + (ENEMY_WIDTH + ENEMIES_GAP) * (c - 1), enemies_y_start + (ENEMY_HEIGHT + ENEMIES_GAP) * (r - 1), enemies_rows[#enemies_rows - r + 1])
+            local enemy = Enemy(enemies_pic, enemies_x_start + (ENEMY_WIDTH + ENEMIES_GAP) * (c - 1), enemies_y_start + (ENEMY_HEIGHT + ENEMIES_GAP) * (r - 1), enemies_rows[#enemies_rows - r + 1])
             table.insert(tmp_enemies, enemy)
         end
-        table.insert(enemies, tmp_enemies)
+        table.insert(self.enemies, tmp_enemies)
     end
 
     -- Enemies Projectiles
-    e_projectiles_wait = ENEMIES_PROJECTILES_WAIT
-    e_projectiles_timer = 0
-    e_projectiles = {}
+    self.e_projectiles_wait = ENEMIES_PROJECTILES_WAIT
+    self.e_projectiles_timer = 0
+    self.e_projectiles = {}
 
     -- Explosions
-    explosions = {}
+    self.explosions = {}
 
     -- Game Over
-    game_over_timer = 0
-    game_over_wait = 3
+    self.game_over_timer = 0
+    self.game_over_wait = 3
 end
 
 function Game:update(dt)
-    if (game_over) then
-        if (gameover.status == "lose") then
-            player.visibility = false
-            generateExplosion(player.x, player.y)
-        end
-        if (game_over_timer >= game_over_wait) then
-            current_page = "Game Over"
-        else
-            game_over_timer = game_over_timer + dt
-        end
+    if (self.game_over) then
+        self:gameOver(dt)
     end
 
-    timersUpdate(dt)
-    player:update(dt)
-    projectilesUpdate(dt)
-    aliensUpdate(dt)
-    eProjectilesUpdate(dt)
-    explosionsUpdate(dt)
+    self:timersUpdate(dt)
+    self.player:update(dt)
+    self:projectilesUpdate(dt)
+    self:aliensUpdate(dt)
+    self:eProjectilesUpdate(dt)
+    self:explosionsUpdate(dt)
 end
 
 function Game:draw()
-    arrayDraw(explosions)
-    playerDraw()
-    arrayDraw(projectiles)
-    enemiesDraw()
-    arrayDraw(e_projectiles)
+    arrayDraw(self.explosions)
+    self:playerDraw()
+    arrayDraw(self.projectiles)
+    self:enemiesDraw()
+    arrayDraw(self.e_projectiles)
 end
 
 -- UPDATE FUNCTIONS --
-function timersUpdate(dt)
+function Game:timersUpdate(dt)
     -- Shoot no more than the maximum number of player's projectiles every "projectiles_wait" seconds
-    if projectiles_timer < PROJECTILES_WAIT then
-        projectiles_timer = projectiles_timer + dt
+    if self.projectiles_timer < PROJECTILES_WAIT then
+        self.projectiles_timer = self.projectiles_timer + dt
     end
     -- Shoot enemies' projectiles every "e_projectiles_wait" seconds
-    if e_projectiles_timer < ENEMIES_PROJECTILES_WAIT then
-        e_projectiles_timer = e_projectiles_timer + dt
+    if self.e_projectiles_timer < ENEMIES_PROJECTILES_WAIT then
+        self.e_projectiles_timer = self.e_projectiles_timer + dt
     end
 end
 
-function projectilesUpdate(dt)
+function Game:projectilesUpdate(dt)
     -- Shoot Player's projectile
-    if love.keyboard.isDown("space") and #projectiles < PROJECTILES_MAX and projectiles_timer >= PROJECTILES_WAIT then
-        projectiles_timer = projectiles_timer - PROJECTILES_WAIT
-        projectile = Projectile(projectile_pic, projectile_sound, player.x + player.width / 2 - PROJECTILES_WIDTH / 2, player.y)
-        table.insert(projectiles, projectile)
+    if love.keyboard.isDown("space") and #self.projectiles < PROJECTILES_MAX and self.projectiles_timer >= PROJECTILES_WAIT then
+        self.projectiles_timer = self.projectiles_timer - PROJECTILES_WAIT
+        local projectile = Projectile(projectile_pic, projectile_sound, self.player.x + self.player.width / 2 - PROJECTILES_WIDTH / 2, self.player.y)
+        table.insert(self.projectiles, projectile)
         playSound(projectile.sound)
     end
 
     -- Move Player's projectiles, if any
-    for i,v in ipairs(projectiles) do
+    for i,v in ipairs(self.projectiles) do
         v:update(dt, -1)
     end
 
     -- Clear player's projectiles (avoiding memory leaks)
-    for i=#projectiles,1,-1 do
-        if projectiles[i].y + projectiles[i].height <= 0 then
-            table.remove(projectiles, i)
+    for i=#self.projectiles,1,-1 do
+        if self.projectiles[i].y + self.projectiles[i].height <= 0 then
+            table.remove(self.projectiles, i)
         end
     end
 end
 
-function aliensUpdate(dt)
+function Game:aliensUpdate(dt)
     -- Aliens movement
-    for i,v in ipairs(enemies) do
+    for i,v in ipairs(self.enemies) do
         for j,enemy in ipairs(v) do
             if (enemy ~= 0) then
-                enemy:update(dt, enemies_direction)
-                if (enemies_direction == 1 and enemy.x + enemy.width > WINDOW_WIDTH) or (enemies_direction == -1 and enemy.x < 0) then
-                    enemies_adjust = true
-                    enemies_direction = enemies_direction * -1
+                enemy:update(dt, self.enemies_direction)
+                if (self.enemies_direction == 1 and enemy.x + enemy.width > WINDOW_WIDTH) or (self.enemies_direction == -1 and enemy.x < 0) then
+                    self.enemies_adjust = true
+                    self.enemies_direction = self.enemies_direction * -1
                 end
                 -- Check for collision with projectiles, if any
                 -- Remove projectile and reset projectile timer
                 -- Remove enemy (set it to zero)
-                for p,projectile in ipairs(projectiles) do
+                for p,projectile in ipairs(self.projectiles) do
                     if (projectile:collision(enemy, -1)) then
-                        projectiles_timer = PROJECTILES_WAIT
-                        table.remove(projectiles, p)
+                        self.projectiles_timer = PROJECTILES_WAIT
+                        table.remove(self.projectiles, p)
 
-                        enemies[i][j]:updateHealth(-1)
-                        if (enemies[i][j].health == 0) then
+                        self.enemies[i][j]:updateHealth(-1)
+                        if (self.enemies[i][j].health == 0) then
                             -- Explosion animation
-                            generateExplosion(enemies[i][j].x, enemies[i][j].y)
+                            self:generateExplosion(self.enemies[i][j].x, self.enemies[i][j].y)
 
-                            enemies[i][j] = 0
+                            self.enemies[i][j] = 0
                             -- Check if the whole column is zero
                             if (j == 1 or j == #v) then
-                                enemies = removeColumn(enemies, j)
+                                self.enemies = removeColumn(self.enemies, j)
                             end
 
                             -- Chech if all enemies are dead
-                            if (#enemies[1] <= 0) then
+                            if (#self.enemies[1] <= 0) then
                                 gameover:resultUpdate()
                                 current_page = "Game Over"
                             else
-                                enemies = updateEnemiesSpeed(enemies, 0.5)
+                                self.enemies = updateEnemiesSpeed(self.enemies, 0.5)
                             end
                         end
 
@@ -161,73 +154,73 @@ function aliensUpdate(dt)
         end
     end
     -- Adjust the enemies in the screen
-    if (enemies_adjust) then
-        enemies = adjustEnemies(enemies, enemies_direction * -1)
-        enemies_adjust = false
+    if (self.enemies_adjust) then
+        self.enemies = adjustEnemies(self.enemies, self.enemies_direction * -1)
+        self.enemies_adjust = false
     end
 
     -- Check Enemies colliding with the player
-    game_over = enemiesCollision(enemies, player)
+    self.game_over = enemiesCollision(self.enemies, self.player)
 end
 
-function eProjectilesUpdate(dt)
+function Game:eProjectilesUpdate(dt)
     -- Create Enemies projectiles
-    if (e_projectiles_timer >= e_projectiles_wait) then
-        e_projectiles_timer = e_projectiles_timer - e_projectiles_wait
+    if (self.e_projectiles_timer >= self.e_projectiles_wait) then
+        self.e_projectiles_timer = self.e_projectiles_timer - self.e_projectiles_wait
         ::random_enemy::
-        local e_row = math.random(1, #enemies)
-        local e_col = math.random(1, #enemies[1])
-        if (enemies[e_row][e_col] == 0) then
+        local e_row = math.random(1, #self.enemies)
+        local e_col = math.random(1, #self.enemies[1])
+        if (self.enemies[e_row][e_col] == 0) then
             goto random_enemy
-        elseif (enemies[e_row][e_col]) then
-            local enemy = enemies[e_row][e_col]
-            e_projectile = Projectile(e_projectile_pic, e_projectile_sound, enemy.x + enemy.width / 2 - PROJECTILES_WIDTH / 2, enemy.y + enemy.height - PROJECTILES_HEIGHT)
+        elseif (self.enemies[e_row][e_col]) then
+            local enemy = self.enemies[e_row][e_col]
+            local e_projectile = Projectile(e_projectile_pic, e_projectile_sound, enemy.x + enemy.width / 2 - PROJECTILES_WIDTH / 2, enemy.y + enemy.height - PROJECTILES_HEIGHT)
             e_projectile.speed = ENEMIES_PROJECTILES_SPEED
-            table.insert(e_projectiles, e_projectile)
+            table.insert(self.e_projectiles, e_projectile)
             playSound(e_projectile_sound)
-            e_projectiles_wait = math.random(1, 3)
+            self.e_projectiles_wait = math.random(1, 3)
         end
     end
 
     -- Move enemies' projectiles, if any
-    for i,p in ipairs(e_projectiles) do
+    for i,p in ipairs(self.e_projectiles) do
         p:update(dt, 1)
         
         -- Check if enemy's projectile collided with player
-        if (p:collision(player, 1)) then
-            table.remove(e_projectiles, i)
-            player:hit(-1)
-            game_over = player:checkDeath()
+        if (p:collision(self.player, 1)) then
+            table.remove(self.e_projectiles, i)
+            self.player:hit(-1)
+            self.game_over = self.player:checkDeath()
         end
     end
 
     -- Clear enemies' projectiles (avoiding memory leaks)
-    for p=#e_projectiles,1,-1 do
-       if (e_projectiles[p].y > WINDOW_HEIGHT) then
-            table.remove(e_projectiles, p)
+    for p=#self.e_projectiles,1,-1 do
+       if (self.e_projectiles[p].y > WINDOW_HEIGHT) then
+            table.remove(self.e_projectiles, p)
        end
     end
 end
 
-function explosionsUpdate(dt)
-    for i=#explosions,1,-1 do
-        explosions[i]:update(dt)
-        if (explosions[i]:done()) then
-            table.remove(explosions, i)
+function Game:explosionsUpdate(dt)
+    for i=#self.explosions,1,-1 do
+        self.explosions[i]:update(dt)
+        if (self.explosions[i]:done()) then
+            table.remove(self.explosions, i)
         end
     end
 end
 
 -- DRAW FUNCTIONS --
-function playerDraw()
-    player:healthDraw()
-    player:draw()
+function Game:playerDraw()
+    self.player:healthDraw()
+    self.player:draw()
 end
 
-function enemiesDraw()
-    for r,v in ipairs(enemies) do
+function Game:enemiesDraw()
+    for r,v in ipairs(self.enemies) do
         for c,enemy in ipairs(v) do
-            if (enemies[r][c] ~= 0) then
+            if (self.enemies[r][c] ~= 0) then
                 enemy:draw()
             end
         end
@@ -235,7 +228,22 @@ function enemiesDraw()
 end
 
 -- GENERATE ENTITIES --
-function generateExplosion(x, y)
-    table.insert(explosions, Explosion(explosion_pic, x, y))
+function Game:generateExplosion(x, y)
+    table.insert(self.explosions, Explosion(explosion_pic, x, y))
     playSound(explosion_sound)
+end
+
+-- GAME LOSS --
+function Game:gameOver(dt)
+    if (gameover.status == "lose") then
+        if (not self.player_dead) then
+            self.player.visibility = false
+            self:generateExplosion(self.player.x, self.player.y)                
+        end
+        if (self.game_over_timer >= self.game_over_wait) then
+            current_page = "Game Over"
+        else
+            self.game_over_timer = self.game_over_timer + dt
+        end
+    end
 end
